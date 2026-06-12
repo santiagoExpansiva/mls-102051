98955b6e6deed716dbf8623931ecbbac2c1071f6
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-06-12 18:22:50 +0000

    group-1
    group-1

---
+++ l2/project.ts
@@ -0,0 +1,15 @@
+/// <mls fileReference="_102051_/l2/project.ts" enhancement="_100554_enhancementLit" />
+
+export const projectConfig = {
+    masterFrontEnd: {
+        build: '',
+        start: '',
+        liveView: '',
+    },
+    masterBackEnd: {
+        build: '',
+        start: '',
+        serverView: ''
+    },
+    modules: []
+}
